f3ec499af379b6aee71d3a90ee150575d3d61c29
Leon Kloker <leon@DN0a1fca9e.SUNet>
2024-03-06 11:35:20 -0800

    header updated, gitignore updated

--- acquisolar/frontend/src/views/viewcomponents/header.js
+++ acquisolar/frontend/src/views/viewcomponents/header.js
@@ -1,43 +1,64 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // Header component
 const Header = () => {
-    return (
+  const navigate = useNavigate();
 
-      <header style={styles.header}>
-        <h1 style={styles.title}>ACQUISOLAR</h1>
-        <div>
-          <button style={styles.headerButton}>Upload</button>
-          <button style={styles.headerButton}>View Files</button>
-        </div>
-        
-      </header>
-
-    );
+  const handleUploadClick = () => {
+    console.log('Upload button clicked');
+    navigate('/'); 
+  };
+  
+  const handleFilesClick = () => {
+    console.log('Files button clicked');
+    navigate('/folders'); 
+  };
+  
+  const handleSearchClick = () => {
+    console.log('Search button clicked');
+    navigate('/searchscreen'); 
   };
 
+  return (
+
+    <header style={styles.header}>
+      <h1 style={styles.title}>ACQUISOLAR</h1>
+      <div>
+        <button style={styles.headerButton} onClick={handleUploadClick}>Upload</button>
+        <button style={styles.headerButton} onClick={handleFilesClick}>Files</button>
+        <button style={styles.headerButton} onClick={handleSearchClick}>Search</button>
+      </div>
+      
+    </header>
+
+  );
+};
 
 const styles = {
     header: {
         display: 'flex',
-        justifyContent: 'space-between',
+        justifyContent: 'left',
         alignItems: 'center',
         padding: '20px',
         color: 'black',
         fontSize: '16px',
         backgroundColor: '#FFFFFF',
         fontFamily: 'Helvetica Neue',
-        borderBottom: '1px solid #DEE2E6', 
+        borderBottom: '1px solid #DEE2E6',
       },
     title: {
         margin: 0,
+        marginRight: 250
       },
       headerButton: {
         fontFamily: 'Helvetica Neue',
         border: 0,
         backgroundColor: "white",
-        fontSize: '18px',
+        fontSize: '25px',
         margin: 10,
+        marginRight: 40,
+        cursor: 'pointer',
       },
   };
 
